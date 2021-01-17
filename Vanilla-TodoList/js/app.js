c376f7d0da8ddddaddb6455d48c7436af6292288
Christoph Lindstädt <45950730+DataLind@users.noreply.github.com>
2021-01-17 19:35:48 +0100

    Todo List Layout (only footer and resp. design  is missing)

--- Vanilla-TodoList/js/app.js
+++ Vanilla-TodoList/js/app.js
@@ -1,3 +1,11 @@
 window.addEventListener("DOMContentLoaded", () => {
   console.log("ready");
 });
+
+const submitBtn = document.querySelector(".input-btn");
+submitBtn.addEventListener("click", function (e) {
+  // e.preventDefault();
+});
+
+const inputDate = document.querySelector(".input-date");
+inputDate.setAttribute("value", new Date().toISOString().slice(0, 10));
