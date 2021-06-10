6a448f212ab10a0ec390f10ed6e68c67d2f604c1
Christoph Lindstädt <45950730+DataLind@users.noreply.github.com>
2021-06-10 19:03:29 +0200

    add model and tests

--- src/ts/components/model.ts
+++ src/ts/components/model.ts
@@ -1,3 +1,46 @@
+import Todo from "../entities/todo";
+import Bucket from "../entities/bucket";
+
+interface AppState {
+  buckets: Bucket[];
+}
+
 export default class Model {
-  constructor() {}
+  #state: AppState = {
+    buckets: [],
+  };
+
+  addBucket(title: string = "New Bucket"): void {
+    /**
+     * Adds a bucket to the model
+     * @param title {string} - title of the container
+     */
+    this.#state.buckets = [...this.#state.buckets, new Bucket(title)];
+  }
+
+  private _findBucket(bucketID: string): Bucket {
+    return this.#state.buckets.find((bucket) => bucket.id === bucketID);
+  }
+
+  addTodoToBucket(
+    bucketID: string,
+    todoTitle: string = "New Todo",
+    todoDescription: string = "Todo Description"
+  ): string {
+    const targetBucket = this._findBucket(bucketID);
+    const newTodo = new Todo(targetBucket.title, todoTitle, todoDescription);
+
+    targetBucket.addItemToBucket(newTodo);
+
+    return newTodo.id;
+  }
+
+  removeTodoFromBucket(bucketID: string, todoID: string): void {
+    const targetBucket = this._findBucket(bucketID);
+    targetBucket.removeItemFromBucket(todoID);
+  }
+
+  get buckets(): Bucket[] {
+    return this.#state.buckets;
+  }
 }
