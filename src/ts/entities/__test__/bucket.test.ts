402132ca1dd167b2f0a163c33c76e806967a6413
Christoph Lindstädt <45950730+DataLind@users.noreply.github.com>
2021-06-12 23:38:59 +0200

    bucket functionality + test refinement

--- src/ts/entities/__test__/bucket.test.ts
+++ src/ts/entities/__test__/bucket.test.ts
@@ -3,7 +3,7 @@ import Bucket from "../bucket";
 
 describe("Bucket Test", () => {
   it("should create an empty bucket", () => {
-    const bucket = new Bucket();
+    const bucket = new Bucket("New Bucket");
 
     expect(bucket.title).toBe("New Bucket");
     expect(bucket.items).toEqual([]);
@@ -11,20 +11,36 @@ describe("Bucket Test", () => {
   });
 
   it("should add todo entity to bucket", () => {
-    const bucket = new Bucket();
+    const bucket = new Bucket("New Bucket");
 
-    bucket.addItemToBucket(new Todo("", ""));
+    bucket.addItemToBucket(new Todo("", "", ""));
     expect(bucket.items.length).toBeGreaterThan(0);
     expect(bucket.items[0]).toBeInstanceOf(Todo);
+
+    bucket.addItemToBucket(new Todo("", "Test", ""));
+
+    expect(bucket.items[0].title).toBe("Test");
+
+    bucket.addItemToBucket(new Todo("", "Test End", ""), true);
+
+    expect(bucket.items[bucket.items.length - 1].title).toBe("Test End");
   });
 
   it("should remove todo entity from bucket", () => {
-    const bucket = new Bucket();
+    const bucket = new Bucket("New Bucket");
 
-    bucket.addItemToBucket(new Todo("", ""));
+    bucket.addItemToBucket(new Todo("", "", ""));
     const id = bucket.items[0].id;
 
     bucket.removeItemFromBucket(id);
     expect(bucket.items).toEqual([]);
+  });
+
+  it("should rename the bucket", () => {
+    const bucket = new Bucket("New Bucket");
+
+    bucket.changeBucketTitle("Changed Title");
+
+    expect(bucket.title).toBe("Changed Title");
   });
 });
--- src/ts/entities/bucket.ts
+++ src/ts/entities/bucket.ts
@@ -1,19 +1,25 @@
 import Todo from "./todo";
-import { generateID } from "../utils/utils.js";
+import { generateID } from "../utils/utils";
 
 export default class Bucket {
   readonly id: string = generateID();
   items: Todo[] = [];
 
-  constructor(public title: string = "New Bucket") {
+  constructor(public title: string, items: Todo[] = []) {
     this.title = title;
+    this.items = items;
   }
 
-  addItemToBucket(this: Bucket, todo: Todo) {
-    this.items = [...this.items, todo];
+  addItemToBucket(this: Bucket, todo: Todo, toEnd = false) {
+    if (toEnd) this.items = [...this.items, todo];
+    if (!toEnd) this.items = [todo, ...this.items];
   }
 
   removeItemFromBucket(id: string) {
     this.items = this.items.filter((todo) => todo.id !== id);
+  }
+
+  changeBucketTitle(newTitle: string) {
+    this.title = newTitle;
   }
 }
